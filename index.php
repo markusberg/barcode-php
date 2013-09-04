9c736c12201734fcb40dcc28cb6f131b453944ad
Markus Berg <markus@kelvin.nu>
2013-09-04 15:02:32 +0200

    Initial import

---
+++ index.php
@@ -0,0 +1,218 @@
+<?php
+include "../../site/Page.class.php";
+$page = new Page();
+$page->setTitle("Barcode generator");
+$page->printHeader();
+
+?>
+<style type="text/css">
+th {
+    text-align: right;
+    white-space: nowrap;
+}
+#advanced {
+    padding: 0.3em;
+    cursor: pointer;
+    font-style: italic;
+    text-decoration: underline;
+    color: blue;
+}
+#advancedBlock {
+    border: 1px solid grey;
+    background: #efefef;
+    display: none;
+    padding: 0.5em 1em;
+    margin-bottom: 1em;
+}
+#prefix {
+    text-transform: uppercase;
+}
+#warning {
+    color: red;
+    font-style: italic;
+    display: none;
+}
+</style>
+
+<script type="text/javascript">
+function zeropad(n, width) {
+    n = n + '';
+    return n.length >= width ? n : new Array(width - n.length + 1).join('0') + n;
+}
+
+function toggleDisplay( sName ) {
+    var domName = document.getElementById( sName );
+    domName.style.display = ( domName.style.display=="block" ? "none" : "block" );
+}
+
+function toggleAdvanced() {
+    if ( domAdvancedBlock.style.display=="block" ) {
+        domAdvancedBlock.style.display="none";
+        domAdvanced.innerHTML="Display advanced layout options";
+    } else {
+        domAdvancedBlock.style.display="block";
+        domAdvanced.innerHTML="Hide advanced layout options";
+    }
+}
+
+function reloadSample() {
+    var image = document.createElement('img');
+    var fileName = (domTape.value=="dlt" ? "dlt" : "lto") + "-" + domTextOrientation.value + "-" + (domColors.checked ? "color" : "bw") + ".png";
+    domSample.setAttribute("src", fileName);
+}
+
+function sanityCheck() {
+    // Ensure that startno contains only digits
+    domStartno.value = domStartno.value.replace(/\D/g, '');
+
+    // Sanity check the length of the label
+    if (domPrefix.value.length + domStartno.value.length > 6) {
+        domWarning.style.display="block";
+    } else {
+        domWarning.style.display="none";
+    }
+}
+
+function init() {
+    domAdvanced = document.getElementById('advanced');
+    domAdvancedBlock = document.getElementById('advancedBlock');
+    domAdvanced.addEventListener("click", toggleAdvanced, false);
+
+    domTape = document.getElementById("tape");
+    domPrefix = document.getElementById("prefix");
+    domStartno = document.getElementById("startno");
+    domWarning = document.getElementById("warning");
+    domTextOrientation = document.getElementById("textOrientation");
+    domColors = document.getElementById("colors");
+
+    // Display appropriate sample image depending on current selections
+    domTape.addEventListener("change", reloadSample);
+    domTextOrientation.addEventListener("change", reloadSample);
+    domColors.addEventListener("change", reloadSample);
+
+    // Sanity check length of labels
+    domPrefix.addEventListener("keyup", sanityCheck);
+    domStartno.addEventListener("keyup", sanityCheck);
+
+    // Reload sample image in case of back-button-press
+    domSample = document.getElementById('sample');
+    reloadSample();
+}
+
+var domTape;
+var domPrefix;
+var domStartno;
+var domWarning;
+
+var domTextOrientation;
+var domColors;
+
+var domAdvanced;
+var domAdvancedBlock;
+
+var domSample;
+
+window.onload = init;
+
+</script>
+
+<?php
+
+$page->printMenu();
+
+?>
+
+        <h1>Barcode label generator</h1>
+        <p>Use this form to generate barcodes for your tape library. Each LTO label can contain at most six characters
+        plus a two-character media identifier (L3, L4, etc.). A DLT label is limited to six characters.</p>
+        <p>All feedback is welcome.</p>
+        <form name="barcodeGenerator" method="post" action="print.php">
+            <table>
+                <tr>
+                    <th>Tape type: </th>
+                    <td><select name="tape" id="tape">
+                        <option value="dlt">Super DLT</option>
+                        <option value="l3" selected="selected">LTO-3</option>
+                        <option value="l4">LTO-4</option>
+                        <option value="l5">LTO-5</option>
+                        <option value="l6">LTO-6</option>
+                    </select></td>
+                </tr>
+                <tr>
+                    <th>Prefix: </th>
+                    <td><input type="text" maxlength=6 name="prefix" id="prefix" value="ZF" /></td>
+                    <td rowspan=2><div id="warning">Warning: a label can at most contain six characters</div></td>
+                </tr>
+                <tr>
+                    <th>Starting number: </th>
+                    <td><input type="text" maxlength=6 name="startno" id="startno" value="0001" /></td>
+                </tr>
+                </tr>
+                    <th>Sample: </th>
+                    <td colspan=2><img src="lto-vertical-bw.png" id="sample"/></td>
+                <tr>
+            </table>
+            
+            <p id="advanced">Display advanced layout options</p>
+
+            <div id="advancedBlock">
+                <table>
+                    <!--
+                    <tr>
+                        <th>Checksum: </th>
+                        <td><input type="checkbox" name="checksum" value="true" id="checksum" /><label for="checksum"> (not all tape libraries require this)</label></td>
+                    </tr>
+                    -->
+                    <tr>
+                        <th>Borders: </th>
+                        <td><input type="checkbox" name="borders" value="true" id="borders" checked="checked" /><label for="borders"> labels are printed with borders</label></td>
+                    </tr>
+                    <tr>
+                        <th>Colors: </th>
+                        <td><input type="checkbox" name="colors" value="true" id="colors" /><label for="colors"> labels are colorized (tri-optic vibrant compatible)</label></td>
+                    </tr>
+                    <tr>
+                        <th>Text orientation: </th>
+                        <td><select name="textOrientation" id="textOrientation">
+                            <option value="vertical">Vertical</option>
+                            <option value="horizontal">Horizontal</option>
+                        </select></td>
+                    </tr>
+                    <tr>
+                        <th>Page size: </th>
+                        <td><select name="pageSize" />
+                            <option value="a4">A4</option>
+                            <option value="letter">Letter</option>
+                        </select></td>
+                    </tr>
+                    <tr>
+                        <th>Columns: </th>
+                        <td><input type="text" name="cols" value="2" /></td>
+                    </tr>
+                    <tr>
+                        <th>Rows: </th>
+                        <td><input type="text" name="rows" value="16" /></td>
+                    </tr>
+                    <tr>
+                        <th>Page left margin: </th>
+                        <td><input type="text" name="marginPageLeft" value="18.5" />mm</td>
+                    </tr>
+                    <tr>
+                        <th>Page top margin: </th>
+                        <td><input type="text" name="marginPageTop" value="22" />mm</td>
+                    </tr>
+                    <tr>
+                        <th>Spacing between columns: </th>
+                        <td><input type="text" name="spacingCol" value="20.5" />mm</td>
+                    </tr>
+                    <tr>
+                        <th>Spacing between rows: </th>
+                        <td><input type="text" name="spacingRow" value="0" />mm</td>
+                    </tr>
+                </table>
+            </div>
+            <input type="submit" value="Generate PDF" />
+        </form>
+
+<?php
+$page->printFooter();
---
+++ print.php
@@ -0,0 +1,158 @@
+<?php
+
+$tape = isset($_POST['tape']) ? $_POST['tape'] : 'l3';
+
+$prefix = isset($_POST['prefix']) ? strtoupper($_POST['prefix']) : '';
+$startno = isset($_POST['startno']) ? $_POST['startno'] : '0000';
+$checksum = ( isset($_POST['checksum']) && $_POST['checksum'] == 'true' ) ? true : false;
+$borders = ( isset($_POST['borders']) && $_POST['borders'] == 'true' ) ? true : false;
+$colors = ( isset($_POST['colors']) && $_POST['colors'] == 'true' ) ? true : false;
+$textOrientation = ( isset($_POST['textOrientation']) && $_POST['textOrientation'] == 'horizontal' ) ? 'horizontal' : 'vertical';
+$pageSize = ( isset($_POST['pageSize']) && $_POST['pageSize'] == 'letter' ) ? 'Letter' : 'A4';
+$rows = ( isset($_POST['rows']) && is_numeric($_POST['rows']) ) ? $_POST['rows'] : 1;
+$cols = ( isset($_POST['cols']) && is_numeric($_POST['cols']) ) ? $_POST['cols'] : 1;
+
+$marginPageLeft = ( isset($_POST['marginPageLeft']) && is_numeric($_POST['marginPageLeft']) ) ? $_POST['marginPageLeft'] : 0;
+$marginPageTop = ( isset($_POST['marginPageTop']) && is_numeric($_POST['marginPageTop']) ) ? $_POST['marginPageTop'] : 0;
+
+$spacingCol = ( isset($_POST['spacingCol']) && is_numeric($_POST['spacingCol']) ) ? $_POST['spacingCol'] : 0;
+$spacingRow = ( isset($_POST['spacingRow']) && is_numeric($_POST['spacingRow']) ) ? $_POST['spacingRow'] : 0;
+
+// Label dimensions taken from: 
+// http://www.vinastar.com/docs/tls/TLSRLS_Barcode_Labels_Specs.pdf
+$wLabel = $tape == 'dlt' ? 57 : 76.2;    // max 57,2 mm & 78,2 mm
+$hLabel = $tape == 'dlt' ? 20 : 15.875;   // max 20,8 mm & 15,5 mm
+
+$wBarcode = $tape == 'dlt' ? 47 : 65;  // max 47,0 mm & 69,9 mm
+$hBarcode = $tape == 'dlt' ? 14 : 10.5; // min 10,2 mm & 9,9 mm
+
+// the radius of the curvature of the label corners
+$radius = ($tape == 'dlt' ? 0 : 2.5);
+
+
+$aColors = array( 0 => array(182, 40,  42),
+                  1 => array(252, 227, 75),
+                  2 => array(149, 196, 83),
+                  3 => array(1,   165, 226),
+                  4 => array(160, 167, 181),
+                  5 => array(216, 125, 54),
+                  6 => array(226, 121, 152),
+                  7 => array(102, 166, 69),
+                  8 => array(245, 182, 84),
+                  9 => array(113, 88,  128) );
+
+require_once('tcpdf/tcpdf.php');
+
+// create new PDF document
+$pdf = new TCPDF('P', 'mm', $pageSize);
+$pdf->SetMargins($marginPageLeft, $marginPageTop);
+$pdf->SetAutoPageBreak(false);
+
+// set document information
+$pdf->SetCreator('http://kelvin.nu/software/barcode/');
+$pdf->SetAuthor('Markus Berg');
+$pdf->SetTitle('Barcodes');
+$pdf->SetSubject('Barcodes');
+$pdf->SetKeywords('barcode, code39, dlt, lto');
+
+
+$pdf->setPrintHeader(false);
+$pdf->setPrintFooter(false);
+
+
+// add a page
+$pdf->AddPage();
+
+// define barcode style
+$style = array(
+    'border' => false,
+    'text' => false,
+    'stretch' => false
+);
+
+$iStartNo = (int)$startno;
+
+// Define the "boxes" that the letters and numbers will be printed in
+$iBoxes = ($tape=='dlt' ? 6 : 7);
+$wBox = ($wLabel-( 2*$radius ))/$iBoxes;
+$hBox = $hLabel-$hBarcode;
+
+// The text can be larger when the text orientation is vertical
+$fontSize = ($hBox-2)/25.4*72;
+$pdf->SetFont('helvetica', 'B');
+
+
+for ($c=0; $c<$cols; $c++) {
+    $pdf->setPage(1);
+    for ($r=0; $r<$rows; $r++) {
+        // Calculate the correct position on the page, and move to it
+        $posX = $marginPageLeft + $c*( $spacingCol + $wLabel );
+        $posY = $marginPageTop + $r*( $spacingRow + $hLabel );
+        $pdf->SetXY($posX, $posY);
+
+        // Truncate text to maximum allowed
+        $txt = $prefix . sprintf("%0" . strlen($startno) . "d", $iStartNo++);
+        $txt = substr($txt, 0, 6);
+
+        if ($borders) {
+            $pdf->RoundedRect($posX, $posY, $wLabel, $hLabel, $radius);
+        }
+
+
+        // Print contents of boxes
+        if ($textOrientation == 'vertical') {
+            // Set a proper font size
+            $pdf->SetFontSize($fontSize*1.6);
+            // begin at bottom left, and start a rotation
+            $pdf->SetXY($posX+$radius, $posY+$hLabel);
+            $pdf->StartTransform();
+            $pdf->Rotate(90);
+        } else {
+            // Set a proper font size
+            $pdf->SetFontSize($fontSize*1.3);
+            $pdf->SetXY($posX+$radius, $posY+$hBarcode);
+        }
+
+        for ($i=0; $i<strlen($txt); $i++) {
+            $letter = substr($txt, $i, 1);
+            if ($colors && is_numeric($letter)) {
+                $pdf->SetFillColorArray($aColors[$letter]);
+            } else {
+                $pdf->SetFillColor(255, 255,255);
+            }
+            if ($textOrientation=='vertical') {
+                $pdf->Cell($hBox, $wBox, $letter, 0, 2, 'C', true);
+            } else {
+                $pdf->Cell($wBox, $hBox, $letter, 0, 0, 'C', true);
+            }
+        }
+        $pdf->SetFillColor(255, 255,255);
+        if ($textOrientation=='vertical') {
+            $pdf->StopTransform();
+        }
+        if ($tape != 'dlt') {
+            // Print media name in a box of its own
+            $pdf->SetFontSize($fontSize);
+            if ($textOrientation == 'vertical') {
+                // begin at bottom left, and start a rotation
+                $pdf->SetXY($posX+$radius+$wBox*6, $posY+$hLabel);
+                $pdf->StartTransform();
+                $pdf->Rotate(90);
+                $pdf->Cell($hBox, $wBox, strtoupper($tape), 0, 0, 'C', true);
+                $pdf->StopTransform();
+            } else {
+                $pdf->SetX($posX+$radius+$wBox*6);
+                $pdf->Cell($wBox, $hBox, strtoupper($tape), 0, 0, 'C', true);
+            }
+            $txt .= strtoupper($tape);
+        }
+        $pdf->write1DBarcode( $txt, 'C39', $posX, $posY, $wLabel, $hBarcode, '', $style);
+        // Draw boxes
+        for ($i=0; $i<$iBoxes; $i++) {
+            $pdf->Rect($posX+$radius+$i*$wBox, $posY+$hBarcode, $wBox, $hBox);
+        }
+    }
+}
+
+$pdf->Output('barcodes.pdf', 'I');
+
